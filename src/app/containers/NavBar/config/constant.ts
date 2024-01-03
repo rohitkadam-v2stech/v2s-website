5019f788889a7ab2f4ef6681ba331c6c117e46f7
lalswift <riddhesh.v2stech@gmail.com>
2024-01-03 17:03:34 +0530

    feat: Added AppBarw in application with routing

---
+++ src/app/containers/NavBar/config/constant.ts
@@ -0,0 +1,22 @@
+export const NAVBAR_ROUTES = [
+  {
+    label: "Home",
+    path: "/",
+  },
+  {
+    label: "About Us",
+    path: "/about-us",
+  },
+  {
+    label: "Case Studies",
+    path: "/case-studies",
+  },
+  {
+    label: "Career",
+    path: "/career",
+  },
+  {
+    label: "Contact Us",
+    path: "/contact-us",
+  },
+] as const;
---
+++ src/app/styles/colors.ts
@@ -0,0 +1,6 @@
+const COLORS = {
+  PRIMARY_BLUE: "#33a7e1",
+  BLACK: "#000",
+};
+
+export default COLORS;
