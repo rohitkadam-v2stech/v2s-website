87b7d8b0f0501c2883c10653d5b425a45e28f95a
rohitkadam-v2stech <rohitkadam.v2stech@gmail.com>
2024-01-09 14:48:12 +0530

    feat: client logos added

---
+++ src/app/modules/Home/components/OurClients/constants.ts
@@ -0,0 +1,19 @@
+import fissaraLogo from "../../../../assets/clientLogos/fissara.png";
+import carebeansLogo from "../../../../assets/clientLogos/carebeans-grey.png";
+import digiallyLogo from "../../../../assets/clientLogos/digially-logo3-1.png";
+import groupLogo from "../../../../assets/clientLogos/group-180.png";
+import pinelabsLogo from "../../../../assets/clientLogos/pinelabs-logo3-1.png";
+import qfixLogo from "../../../../assets/clientLogos/qfix-logo3-1.png";
+import ripplehireLogo from "../../../../assets/clientLogos/ripplehire-logo.png";
+import surfaceLogo from "../../../../assets/clientLogos/surface1.png";
+
+export const CLIENT_LOGOS: Array<typeof fissaraLogo> = [
+  fissaraLogo,
+  carebeansLogo,
+  digiallyLogo,
+  groupLogo,
+  pinelabsLogo,
+  qfixLogo,
+  ripplehireLogo,
+  surfaceLogo,
+];
