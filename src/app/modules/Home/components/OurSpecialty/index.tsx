4cc96b3994a6aacc7655a804682a984f28b1e1d9
rohitkadam-v2stech <rohitkadam.v2stech@gmail.com>
2024-01-09 11:56:01 +0530

    feat: home screen our speciality section added

--- src/app/modules/Home/components/OurSpecialty/index.tsx
+++ src/app/modules/Home/components/OurSpecialty/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Paper, Stack, Typography } from "@mui/material";
+import { Box, Grid, Paper, Stack, Typography, alpha } from "@mui/material";
 import COLORS from "../../../../styles/colors";
 import advisingOnTech from "../../../../assets/Advising on Technology.png";
 
@@ -44,9 +44,9 @@ const OurSpeciality = () => {
       <Stack
         justifyContent="space-between"
         alignItems="center"
-        spacing={2}
+        spacing={4}
         py={2}
-        mx={50}
+        mx={44}
         sx={{ borderRadius: 4 }}
       >
         {OUR_SPECIALTY.map((item, id) => (
@@ -55,48 +55,59 @@ const OurSpeciality = () => {
             sx={{
               borderRadius: 20.5,
               width: "100%",
-              bgcolor: COLORS.LIGHT_BLUE,
+              bgcolor: alpha(COLORS.LIGHT_BLUE, 0.4),
+              color: COLORS.DARK_BLUE,
             }}
             elevation={5}
           >
-            <Stack
-              direction="row"
-              justifyContent="space-around"
-              alignItems="center"
-            >
-              <Stack
-                direction="row"
-                sx={{
-                  borderRadius: 20.5,
-                }}
-                bgcolor={COLORS.PRIMARY_BLUE}
+            <Grid container>
+              <Grid
+                item
+                xs={7}
                 alignItems="center"
+                justifyContent="left"
+                container
+                bgcolor={COLORS.LIGHT_BLUE}
+                borderRadius={20.5}
               >
-                <Box
-                  sx={{
-                    borderRadius: 20.5,
-                    py: 3.5,
-                    px: 4,
-                  }}
-                  bgcolor={COLORS.WHITE}
+                <Stack
+                  direction="row"
+                  spacing={4}
+                  justifyContent="center"
+                  alignItems="center"
                 >
-                  <img src={advisingOnTech} height={53} width={53} alt="" />
-                </Box>
-                <Typography fontSize="21px" fontWeight="600" lineHeight="49px">
-                  {item.title}
-                </Typography>
-              </Stack>
-              <Box
-                sx={{
-                  borderTopRightRadius: 20.5,
-                  borderBottomRightRadius: 20.5,
-                }}
+                  <Box
+                    sx={{
+                      borderRadius: 20.5,
+                      py: 3.5,
+                      px: 4,
+                    }}
+                    bgcolor={COLORS.WHITE}
+                  >
+                    <img src={advisingOnTech} height={53} width={53} alt="" />
+                  </Box>
+                  <Typography
+                    fontSize="21px"
+                    fontWeight="600"
+                    lineHeight="49px"
+                  >
+                    {item.title}
+                  </Typography>
+                </Stack>
+              </Grid>
+              <Grid
+                item
+                xs={5}
+                alignItems="center"
+                justifyContent="left"
+                container
+                p={4}
               >
                 <Typography fontSize="21px" fontWeight="600" lineHeight="49px">
                   {item.subtitle}
                 </Typography>
-              </Box>
-            </Stack>
+              </Grid>
+            </Grid>
           </Paper>
         ))}
       </Stack>
