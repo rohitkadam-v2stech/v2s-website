918c19a5c61ec939194f828fb1078a47b3216a43
rohitkadam-v2stech <rohitkadam.v2stech@gmail.com>
2024-01-09 15:55:56 +0530

    feat: slider added

---
+++ src/app/components/Slider/constants.ts
@@ -0,0 +1,38 @@
+import { type Settings as ReactSliderProps } from "react-slick";
+
+export const SLIDER_DEFAULT_PROPS: ReactSliderProps = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  arrows: false,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  initialSlide: 0,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        initialSlide: 0,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
--- src/app/modules/AboutUs/components/OurAssociation/index.tsx
+++ src/app/modules/AboutUs/components/OurAssociation/index.tsx
@@ -1,13 +1,27 @@
-import { Box } from "@mui/material";
-import SliderWithHeading from "../../../../components/SliderWithHeading";
+import { Box, Stack, Typography } from "@mui/material";
+import Slider from "../../../../components/Slider";
 import { OUR_ASSOCIATION_IMAGES_LIST } from "../../../../config/constants";
+import COLORS from "../../../../styles/colors";
+import { messages } from "./config/messages";
 
 const OurAssociation: React.FC = () => (
   <Box px="6%" py={8} mb={16}>
-    <SliderWithHeading
-      heading="Our Association"
-      data={OUR_ASSOCIATION_IMAGES_LIST}
-    />
+    <Typography
+      fontSize={30}
+      fontWeight={700}
+      color={COLORS.DARK_BLUE}
+      textAlign="center"
+      mb={6}
+    >
+      {messages.ourAssociations}
+    </Typography>
+    <Slider slidesToShow={4}>
+      {OUR_ASSOCIATION_IMAGES_LIST.map((item) => (
+        <Stack key={item}>
+          <img src={item} alt="slider-images" />
+        </Stack>
+      ))}
+    </Slider>
   </Box>
 );
 
