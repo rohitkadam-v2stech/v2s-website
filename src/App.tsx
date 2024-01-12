872f04c9e58b38f43e6fa94cb3f9941e57e53682
lalswift <riddhesh.v2stech@gmail.com>
2024-01-12 10:44:58 +0530

    feat: data changed for case studies json

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
+import { useEffect } from "react";
 import RoutingProvider from "./app/Routing";
 import EntryPoint from "./app/containers/EntryPoint";
+import { useLocation } from "react-router-dom";
 
-const App = () => (
-  <EntryPoint>
-    <RoutingProvider />
-  </EntryPoint>
-);
+const App = () => {
+  const ScrollToTop = () => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+  };
+
+  return (
+    <EntryPoint>
+      <ScrollToTop />
+      <RoutingProvider />
+    </EntryPoint>
+  );
+};
 
 export default App;
