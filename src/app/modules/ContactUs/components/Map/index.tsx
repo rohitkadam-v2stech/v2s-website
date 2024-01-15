7db30beab840deccd8b98aad5a4b2f67c0edcfb6
lalswift <riddhesh.v2stech@gmail.com>
2024-01-15 12:41:15 +0530

    feat: contact us screen added

---
+++ src/app/modules/ContactUs/components/Map/index.tsx
@@ -0,0 +1,12 @@
+const Map: React.FC = () => (
+  <>
+    <iframe
+      title="google-maps-location"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d358475.27674089436!2d73.0399537843815!3d19.20780147350815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b9272af7ceb3%3A0x13f3788d4a27fef9!2sV2STech%20Solutions%20Pvt.%20Ltd.!5e0!3m2!1sen!2sin!4v1704888284094!5m2!1sen!2sin"
+      style={{ border: 0, width: "100%", height: "70vh" }}
+      aria-hidden="false"
+    />
+  </>
+);
+
+export default Map;
---
+++ src/app/modules/ContactUs/config/messages.ts
@@ -0,0 +1,11 @@
+export const messages = {
+  contactUs: "Contact Us",
+  contactUsDescription:
+    "Give us a call or drop by anytime, we endeavour to answer all enquiries within 24 hours on business days. We will be happy to answer your questions.",
+  ourOffices: "Our Offices",
+  headQuarters: "Headquarters",
+  officeAddress:
+    "501, 9 Mansi, Cross Lane no.1, Ram Maruti road, near Gaon devi ground, Thane (West), Maharashtra, India – 400602",
+  businessQueries: "Business Queries",
+  salesEmail: "sales@v2stech.com",
+};
