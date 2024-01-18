cd295ef428e8c77534acb072507de358ac429631
lalswift <riddhesh.v2stech@gmail.com>
2024-01-18 12:41:08 +0530

    small change in case studies details

--- src/app/modules/CaseStudies/components/Details/index.tsx
+++ src/app/modules/CaseStudies/components/Details/index.tsx
@@ -17,36 +17,6 @@ const CaseStudiesDetails: React.FC = () => {
 
       {state?.projectScreenshots && <ProjectImagesSlider state={state} />}
 
-      {/* {state?.projectScreenshots?.length <= 1 ? (
-        <Box
-          px="6%"
-          py={8}
-          display="flex"
-          flexDirection="row"
-          justifyContent="center"
-        >
-          <img
-            src={state?.projectScreenshots[0]}
-            alt={`project-screenshot-1`}
-            height={340}
-          />
-        </Box>
-      ) : (
-        <Box px="6%" py={8}>
-          <Stack px={"20%"} py={4} justifyContent="center">
-            <Slider slidesToShow={isOnMobile ? 1 : 1}>
-              {state?.projectScreenshots?.map((item: any, index: number) => (
-                <img
-                  src={item}
-                  alt={`project-screenshot-${index + 1}`}
-                  // height={400}
-                />
-              ))}
-            </Slider>
-          </Stack>
-        </Box>
-      )} */}
-
       <AboutClients data={state} />
       <ProblemStatement data={state} />
     </>
