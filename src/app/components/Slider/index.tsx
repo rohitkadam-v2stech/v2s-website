1bd3b16a4489923a2b15ed305fb43e2dcbce97e5
rohitkadam-v2stech <rohitkadam.v2stech@gmail.com>
2024-01-30 18:18:59 +0530

    feat: slider icon changes

--- src/app/components/Slider/index.tsx
+++ src/app/components/Slider/index.tsx
@@ -1,6 +1,8 @@
 import ReactSlider, { type Settings as ReactSliderProps } from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
+import "./style.css";
 import { SLIDER_DEFAULT_PROPS } from "./constants";
 
 const Slider: React.FC<ReactSliderProps> = (props) => (
